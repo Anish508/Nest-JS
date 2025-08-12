a30948be2cc0625b59dd2c278a10bea6e9a71ad0
Anish508 <anishbarke9741@gmail.com>
2025-08-12 17:32:51 +0530

    Completed initial project setup of file upload project

--- file-upload/src/app.module.ts
+++ file-upload/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { FileUploadModule } from './file-upload/file-upload.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [FileUploadModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
